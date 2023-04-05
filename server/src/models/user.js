8b5c0b0c1644f297f352ca7dbc4a895a1b2b3d44
UtkarshAhuja2003 <ahujautkarsh80@gmail.com>
2023-04-06 02:00:34 +0530

    last try

--- server/src/models/user.js
+++ server/src/models/user.js
@@ -7,6 +7,9 @@ const findOrCreate=require("mongoose-findorcreate");
 
 
 const userSchema = new mongoose.Schema({
+    username:{
+        type:String
+    },
     name:{
         type:String
     },
