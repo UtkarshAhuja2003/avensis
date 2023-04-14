58ea14f8ffb7bfaf2abb4970859e2f947866bbf5
UtkarshAhuja2003 <ahujautkarsh80@gmail.com>
2023-04-14 12:37:13 +0530

    Revert "fetch issue"
    This reverts commit cc2c7e08236a62f8be65fe810b7c0990846302ce.

--- client/src/components/common/Navbar/index.jsx
+++ client/src/components/common/Navbar/index.jsx
@@ -22,9 +22,7 @@ const Navbar = () => {
   //   setUser(cookie.get("name",{ domain: ''}));
   // }
   useEffect(() => {
-    fetch('https://avensis-backend.msit.in/api/current_user', {
-      credentials: 'include'
-    })
+    fetch('https://avensis-backend.msit.in/api/current_user')
     .then(response => response.json())
     .then(user => {
       console.log('User info:', user);
@@ -35,7 +33,6 @@ const Navbar = () => {
     .catch(error => {
       console.error('Error:', error);
     });
-
   
   }, []);
   
